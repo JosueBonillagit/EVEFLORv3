383b5cf264ccc623b89fb8d1ecd9e7516f419e65
AlanMatute7 <alan.matute@unah.hn>
2024-04-18 00:48:22 -0600

    Actualizacion_Tablas_AlanMatute
    Se hizo los 20 reportes

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'almasaeed2010/adminlte',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '5766a0e01480a28b79bf9d8a60b7fbf0281b28b1',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'almasaeed2010/adminlte' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '5766a0e01480a28b79bf9d8a60b7fbf0281b28b1',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
